9f4628c8f45d86cea895743fc7f2d6ff6c8f149e
Angel Breaux <abreaux@detaso.com>
2021-01-07 11:26:00 -0600

    Create announce lottery winner test

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -1,5 +1,6 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'mocha/minitest'
 require './lib/exhibit'
 require './lib/patron'
 require './lib/museum'
@@ -150,5 +151,36 @@ class MuseumTest < Minitest::Test
 
     assert_instance_of Patron, dmns.draw_lottery_winner(imax)
     assert_nil dmns.draw_lottery_winner(gems_and_minerals)
+  end
+
+  def test_announce_lottery_winner
+    dmns = Museum.new("Denver Museum of Nature and Science")
+
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(imax)
+
+    patron_1 = Patron.new("Bob", 0)
+    patron_1.add_interest("IMAX")
+    patron_1.add_interest("Gems and Minerals")
+
+    patron_2 = Patron.new("Sally", 20)
+    patron_2.add_interest("IMAX")
+
+    patron_3 = Patron.new("Johnny", 15)
+    patron_3.add_interest("IMAX")
+
+    dmns.admit(patron_1)
+    dmns.admit(patron_2)
+    dmns.admit(patron_3)
+
+    winner = mock
+    winner.stubs(:announcement).returns("Bob has won the IMAX exhibit lottery")
+
+    no_winner = "No winners for this lottery"
+
+    assert_equal winner.announcement, dmns.announce_lottery_winner(imax)
+    assert_equal no_winner, dmns.announce_lottery_winner(gems_and_minerals)
   end
 end

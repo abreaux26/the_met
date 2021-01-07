d7ef2602c918329c1092d89ba47691d5a6a497e0
Angel Breaux <abreaux@detaso.com>
2021-01-07 10:17:50 -0600

    Create add exhibit test

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -15,4 +15,15 @@ class MuseumTest < Minitest::Test
     assert_equal "Denver Museum of Nature and Science", dmns.name
     assert_equal [], dmns.exhibits
   end
+
+  def test_add_exhibit
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(imax)
+
+    assert_equal [gems_and_minerals, imax], dmns.exhibits
+  end
 end

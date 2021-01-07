b550472e40944d8b22792366fed46308903fcf7f
Angel Breaux <abreaux@detaso.com>
2021-01-07 10:30:26 -0600

    Create recomment exhibits method

--- lib/museum.rb
+++ lib/museum.rb
@@ -10,4 +10,12 @@ class Museum
   def add_exhibit(exhibit)
     @exhibits << exhibit
   end
+
+  def recommend_exhibits(patron)
+    patron.interests.flat_map do |interest|
+      exhibits.find_all do |exhibit|
+        exhibit.name == interest
+      end
+    end
+  end
 end

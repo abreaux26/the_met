97f894203e8fb01f989da2108244aee4b7815168
Angel Breaux <abreaux@detaso.com>
2021-01-07 11:10:18 -0600

    Create test for draw lottery winner method

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -125,4 +125,30 @@ class MuseumTest < Minitest::Test
 
     assert_equal [patron_1, patron_3], dmns.ticket_lottery_contestants(imax)
   end
+
+  def test_draw_lottery_winner
+    dmns = Museum.new("Denver Museum of Nature and Science")
+
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(imax)
+
+    patron_1 = Patron.new("Bob", 0)
+    patron_1.add_interest("IMAX")
+    patron_1.add_interest("Gems and Minerals")
+
+    patron_2 = Patron.new("Sally", 20)
+    patron_2.add_interest("IMAX")
+
+    patron_3 = Patron.new("Johnny", 5)
+    patron_3.add_interest("IMAX")
+
+    dmns.admit(patron_1)
+    dmns.admit(patron_2)
+    dmns.admit(patron_3)
+
+    assert_instance_of Patron, dmns.draw_lottery_winner(imax)
+    assert_nil dmns.draw_lottery_winner(gems_and_minerals)
+  end
 end

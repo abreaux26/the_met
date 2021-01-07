b53180140f7f73dbe9523b9f9340bc96d52e5904
Angel Breaux <abreaux@detaso.com>
2021-01-07 10:39:54 -0600

    Create patrons by exhibit interest test

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -53,4 +53,30 @@ class MuseumTest < Minitest::Test
 
     assert_equal [patron_1], dmns.patrons
   end
+
+  def test_patrons_by_exhibit_interest
+    dmns = Museum.new("Denver Museum of Nature and Science")
+
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(imax)
+
+    patron_1 = Patron.new("Bob", 20)
+    patron_1.add_interest("IMAX")
+    patron_1.add_interest("Gems and Minerals")
+
+    patron_2 = Patron.new("Sally", 20)
+    patron_2.add_interest("IMAX")
+
+    dmns.admit(patron_1)
+    dmns.admit(patron_2)
+
+    expected = {
+      gems_and_minerals => [patron_1],
+      imax => [patron_1, patron_2]
+    }
+
+    assert_equal expected, dmns.patrons_by_exhibit_interest
+  end
 end
